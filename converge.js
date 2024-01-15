36fc970109511bfa49d38cf388bf8d49c835c2d4
Annelie <100210244+kgl-test-user@users.noreply.github.com>
2024-01-15 10:25:06 -0500

    commented and cleaned up

--- converge.js
+++ converge.js
@@ -1,13 +1,6 @@
-//SHIRLEY WU
-petalPath = "M 0,0 C -10,-10 -10,-40 0,-50 C 10,-40 10,-10 0,0"
 petalPathVar = [];
 petalSize = 20;
 
-
-// 0,0 -5,5 0,-10 5,15 0,-20
-// -5,15 0,-10 5,5
-
-veinPath = "M 0,0 C -10,-10 5,-20 0,-40 C -5,-20 10,-10 0,0";
 veinPathVar = [];
 
 
@@ -25,20 +18,15 @@ const dataPath1 = 'data.json';
 const dataPath2 = 'dryDays.json';
 const dataPath3 = 'wetDays.json';
 
-
-//pink purple red yellow orange 
-// var colorRange = ["#fff7f3","#fff7f3","#fde2df","#fccac8","#fbabb8","#f880aa","#cea0cd","#d9c2df","#e8e1ef","#f7f4f9","#750175","#49006a","#ffffe5","#fff6c0","#fee799","#fece66","#fdac3b","#f58720","#fff5eb"]    
-// var colorRange = ["antiquewhite","antiquewhite", "antiquewhite","antiquewhite","antiquewhite", "antiquewhite","pink", "pink","pink","pink", "pink","pink","magenta","magenta","magenta","purple","red","orange"]
+//the colors should interpolate between each, transitioning smoothly
 var colorRange = ["antiquewhite","pink","purple","red","orange"]
 const fillScale = d3.scaleSequential()
     .interpolator(d3.interpolateRgbBasis(colorRange))
-
-
 var veinRange = ["pink","red","orange"]
 const veinScale = d3.scaleSequential()
     .interpolator(d3.interpolateRgbBasis(veinRange))
 
-
+//loading the data and merging them
 Promise.all([
     d3.json(dataPath1),
     d3.json(dataPath2),
@@ -55,6 +43,7 @@ function draw() {
         .attr("width", w)
         .attr("height", h)
 
+//setting up a bunch of mins and maxes so I can use scales to map the data to visual parameters
     const tempMinMax = d3.extent(data, d => +d.meanTemp);
     console.log(tempMinMax)
     const dryMinMax = d3.extent(data, d => +d.dryDays);
@@ -77,6 +66,7 @@ function draw() {
     //WEIGHTED FACTOR ON TEMPERATURE IS DRY DAYS - WET DAYS
     console.log(comboMinMax)
 
+//setting up scales for color, size and petals
     const hueScale = d3.scaleLinear().range([1, 360]);
     const satScale = d3.scaleLinear().range([1, .7]);
     const ltScale = d3.scaleLinear().range([.9, .6]);
@@ -92,19 +82,22 @@ function draw() {
     function randomInteger(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+
+//preparing the data that each flower / petal will need 
     const flowersData = _.map(data, d => {
         const combo = d.meanTemp * (d.dryDays - d.wetDays)+scaleCO(d.co);
         const numPetals = numPetalScale(d.meanTemp);
         const petSize = sizeScale(combo);
 
         const year = d.year;
-        const temp = d.meanTemp;
-        const dry = d.dryDays;
-        const ano = d.sumTempAnoHigh;
-        const wet = d.wetDays;
-        const airQ = d.airQuality;
+        const temp = d.meanTemp; //annual mean temperature
+        const dry = d.dryDays; //number of consecutive dry days
+        const ano = d.sumTempAnoHigh; //anomaly of temperature for summer months
+        const wet = d.wetDays; //number of consecutive wet days
+        const airQ = d.airQuality; //projected future air quality
 
-        const solar = Math.floor(randomInteger(d.solarRadMedian, d.solarRadMax))
+        //projected future solar radiation - i asked the code to give me random numbers between the median and max 
+        const solar = Math.floor(randomInteger(d.solarRadMedian, d.solarRadMax)) 
         const solSize = solScale(solar)
         return {
             numFlows: _.times(d.wetDays, i => { return { numPetals: numPetals, dry: dry, wet:wet, airQ: airQ, solSize: solSize, solar: solar } }),
@@ -122,7 +115,7 @@ function draw() {
     console.log(flowersData)
 
 
-
+    //thing to download the compiled data
     var download = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(flowersData));
     var a = document.createElement('a');
     a.href = 'data:' + download;
@@ -132,7 +125,8 @@ function draw() {
     container.appendChild(a);
 
 
-
+    //create one flower space per year
+    //use the data accessible at that level of the json nest to drive the color and petal shapes
     const flowers = svg
         .selectAll('g.flower')
         .data(flowersData)
@@ -143,24 +137,28 @@ function draw() {
         })
         .attr('transform', (d, i) => `translate(${(petalSize*8+i * petalSize*5)},${petalSize*4})scale(${d.petSize})`)
         .attr('fill', function(d, i) {
+            //preparing the petal shape array here
+            //the control points for the petal are driven by a random choice between the mean temperature and the projected anomaly
             p1 = 10 + Math.floor(randomInteger(d.temp - d.ano, d.temp + d.ano));
             p2 = 30 + Math.floor(randomInteger(d.temp - d.ano, d.temp + d.ano));
             p3 = 40 + Math.floor(randomInteger(d.temp - d.ano, d.temp + d.ano));
 
 
-            // petalPath = "M 0,0 C -10,-10 -10,-40 0,-50 C 10,-40 10,-10 0,0"  
-           //version of petal path with notch
-           // pPath = ("M 0,0 C -10," + -p1 + " " + -p1 + "," + -p3 + " -5," + (-p3-10) + " C -5," + (-p3-10) + " 0," + (-p3-2) + " 5," + (-p3-10) + " C " + p1 + "," + -p3 + " " + p1 + "," + -p1 + " 0,0");
+            // original: petalPath = "M 0,0 C -10,-10 -10,-40 0,-50 C 10,-40 10,-10 0,0"  
+            //version of petal path with notch
+            // pPath = ("M 0,0 C -10," + -p1 + " " + -p1 + "," + -p3 + " -5," + (-p3-10) + " C -5," + (-p3-10) + " 0," + (-p3-2) + " 5," + (-p3-10) + " C " + p1 + "," + -p3 + " " + p1 + "," + -p1 + " 0,0");
 
             //for the petal path without notch
             pPath = ("M 0,0 C -10," + -p2 + " " + -p1 + "," + -p3 + " 0," + (-p3 - 10) + " C " + p1 + "," + -p3 + " 10," + -p2 + " 0,0");
             petalPathVar.push(pPath);
 
             //for the vein paths
+            //original: veinPath = "M 0,0 C -10,-10 5,-20 0,-40 C -5,-20 10,-10 0,0";
             vPath = ("M 0,0 C -1," + -p1/2 + " " + -p1/3 + "," + -p3 + " 0," + (-p3 - 10) + " C " + p1/3 + "," + -p3 + " 1," + -p1/2 + " 0,0");
             veinPathVar.push(vPath);
 
-            //for the petal fill
+
+            //the petals are filled based on the combo data and saturated based on dry days
             fillScale.domain(comboMinMax)
             satScale.domain(dryMinMax);
 
@@ -180,7 +178,7 @@ function draw() {
         .attr('stroke', 'black')
         .attr('stroke-width', .1);
 
-    //NUMBER OF FLOWERS PER YEAR ACCORDING TO DRY DAYS PER YEAR
+    //NUMBER OF FLOWERS PER YEAR ACCORDING TO WET DAYS PER YEAR, spread out by dry days
     const flows = flowers.selectAll('g.flows')
         .data(d => d.numFlows).enter().append('g')
         .attr('class', function(d) {
@@ -212,7 +210,7 @@ function draw() {
         })
 
 
-    //g elements for veins with stroke
+    //prepare space for each vein path
     const veins = flowers.selectAll('g.veins')
         .data(d => d.numFlows).enter().append('g')
         .attr('class', function(d) {
@@ -220,6 +218,7 @@ function draw() {
         })
         .attr('transform', (d, i) => `translate(${(0)},${i * d.dry*4})`) //spread out by how dry it was
         .attr('stroke', function(d, i) {
+            //veins are colored according to the air quality
             veinScale.domain(airMinMax)
             var thisCol = d3.hsl(veinScale(d.airQ))
             return thisCol;
@@ -236,7 +235,7 @@ function draw() {
         .append('path')
         .attr('class', 'veins')
         .attr('d', function(d, i) {
-            //find the petal path of your year
+            //find the vein path of your year
             var thisYear = d3.select(this.parentNode.parentNode).attr("class");
 
             var whichIndex = thisYear - 2023;
@@ -250,7 +249,9 @@ function draw() {
 
 
 
-    //BULLS EYE
+    //SPACE FOR THE BULLS EYE PATTERN
+    //bulls eye pattern currently envisioned as simply: 
+        //same as petal but sometimes bigger sometimes smaller according to solar radiation of that year
     const bulls = flowers.selectAll('g.bulls')
         .data(d => d.numFlows).enter().append('g')
         .attr('class', function(d) {
@@ -285,152 +286,3 @@ function draw() {
         .attr('stroke-width', .5)
 
 }
-
-
-
-
-// Partial sun Ideal About 3-6 hours sunlight  
-// extreme light conditions lead to lesser growth vigor.
-// For incident solar radiation, the projected change falls between –17 and +2 %. 
-
-
-//      var thisYear = d3.select(this.parentNode.parentNode).attr("class");
-
-// this will need to do to generate variable options within each data year
-// .data((d) =>
-//     d3.range((d.meanTemp - d.sumTempAnoLow), (d.meanTemp + d.sumTempAnoHigh)).map(function(d) {
-//         return d
-//     })
-// )
-
-// Air pollutants such as nitrogen oxides and sulfur dioxide when deposited on soil surfaces can result in acidification processes, altering the soil's pH and nutrient balance.
-
-// how many flowers would occur
-// more rainfall
-// less sunlight    
-// air quality
-// their color
-// preparing data for 1 theoretical year btwn 2080-2100
-// summer = july
-// spring april
-
-// This stress can include a drought or a flood or even a lack of nutrition in the soil, 
-// all of which can dampen the coloration of flowers.
-// Anthocyanins and carotenoids
-// Anthocyanin pigments, stored in the vacuoles of petal cells change color with pH (acidity)
-// The hotter and sunnier the weather gets, the more carotenoids a plant produces, the brighter the oranges and reds will get. When weather cools off, carotenoid production decreases, and colors become softer oranges and yellows.
-
-
-
-
-// When the temperature exceeds 25°C
-// It favors a cool climate in a range of 32 to 77 °F (0 to 25 ℃)
-
-// Partial sun Ideal About 3-6 hours sunlight  extreme light conditions lead to lesser growth vigor.
-// For incident solar radiation, the projected change falls between –17 and +2 %. 
-
-// Moist or wet woods and mossy bogs;
-
-// Flowers: Inflorescence a raceme, terminal, elongate, showy, 
-// with numerous flowers; 
-// floral tube absent; 
-// sepals 7-16 mm long, pinkish purplish, puberulent; 
-// petals unequal, slightly asymmetric, 1-2 cm long, pink-purple, seldom white; 
-// style 1-2 cm long, surpassing the stamens, stigma deeply 4-cleft;
-
-
-// var springTempAvg = [4, 9];
-// var springTempAnomaly = [3.9, 7.86];
-
-// var summerTempAvg = [18, 24]; 
-// var summerTempAnomaly = [2.5, 7.99];
-
-//0-25
-//april 3.1
-//december -1
-// var seasonLength = 8;
-
-// var springWetAvg = [41, 63]; 
-// var springWetAnomaly = [-.35, 21.29]; 
-
-// var summerWetAvg = [70, 107];
-// var summerWetAnomaly = [-16.4, 17.7];
-
-
-// var clScale = d3.scaleDiverging(d3.interpolateRdYlBu).domain([min, max])
-// var clScale = d3.scaleLinear()
-// .domain([min, max])
-// .range(['white', 'pink', 'purple'])
-// .interpolate(d3.interpolateCubehelix)
-// return d3.interpolateCubehelixDefault(pullDown)
-// return 'none';
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//JUST ONE FLOWER PER YEAR
-// flowers
-// .selectAll('path')
-//  .data((d) =>
-//       d.petals
-//   )
-//  .enter()
-//  .append('path')
-//   .attr('d',petalPath)
-//   .attr('fill','none')
-//   // .attr('stroke-width',.1)
-//   .attr('stroke','black')
-//   .attr('transform',function(d,i){
-//      // return `rotate(${d.angle})`
-//   })  
-
-
-// //MAKE BULLS EYE PATTERN
-//     const bullsEye = svg
-//         .selectAll('g.bullsEye')
-//         .data(flowersData)
-//         .enter()
-//         .append('g')
-//         .attr('class','bullsEye')
-//         //scale could relate to data
-//         .attr('transform', (d, i) => `translate(${(petalSize*2+i * petalSize*3)},${h/10})scale(${d.petSize/2})`)
-//         .attr('fill', function(d,i){
-//          hueScale.domain(yearMinMax);
-//          satScale.domain(dryMinMax);
-//          ltScale.domain(anoMinMax);
-//          return d3.hsl(hueScale(d.year),satScale(d.dry),ltScale(d.ano),.9);
-//         })
-//         .attr('stroke','white')
-
-//     //NUMBER OF FLOWERS PER YEAR ACCORDING TO DRY DAYS PER YEAR
-//     const bullsEyeQuant = bullsEye.selectAll('g.quant')
-//         .data(d => d.numFlows).enter().append('g')
-//         .attr('transform', (d, i) => `translate(${(0)},${i * d.dry*8})`)
-
-//     //BULL'S EYE ACCORDING TO TEMPERATURE
-//     bullsEyeQuant
-//         .selectAll('path.uv')
-//         .data(d =>
-//             d3.range(d.tempPetals).map(function(d) {
-//                 return d;
-//             })
-//         )
-//         .enter()
-//         .append('path')
-//         .attr('class','uv')
-//         .attr('d', petalPath)
-//         .attr('transform', function(d, i) {
-//             return `rotate(${multPetals(d)})`
-//         })
